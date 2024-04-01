cf16a677f77e0b88e3450532ed5e16e29824ff17
Sara Amri <93613026+kokkuri3@users.noreply.github.com>
2024-04-01 01:09:50 -0400

    Update TableCreationScript.sql
    Changed ids to UUID instead of int

--- Phase_1/DDL/TableCreationScript.sql
+++ Phase_1/DDL/TableCreationScript.sql
@@ -1,15 +1,15 @@
 -- Car table
 CREATE TABLE Car (
-    car_id INT PRIMARY KEY,
+    car_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
     name VARCHAR(255),
     year INT,
-    mpg INT, 
+    mpg INT,
     horsepower INT
 );
 
 -- User table
 CREATE TABLE User (
-    user_id INT PRIMARY KEY,
+    user_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
     name VARCHAR(255),
     contact_number VARCHAR(15),
     email VARCHAR(255)
@@ -17,25 +17,25 @@ CREATE TABLE User (
 
 -- Customer table
 CREATE TABLE Customer (
-    customer_id INT PRIMARY KEY,
-    user_id INT,
+    customer_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    user_id UUID,
     premium BOOLEAN,
     FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
 -- Employee table
 CREATE TABLE Employee (
-    employee_id INT PRIMARY KEY,
-    user_id INT,
+    employee_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    user_id UUID,
     position ENUM('Manager', 'Salesperson', 'Mechanic'),
     FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
 -- Reservation table
 CREATE TABLE Reservation (
-    reservation_id INT PRIMARY KEY,
-    car_id INT,
-    customer_id INT,
+    reservation_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    car_id UUID,
+    customer_id UUID,
     rental_date DATE,
     return_date DATE,
     FOREIGN KEY (car_id) REFERENCES Car(car_id),
@@ -44,8 +44,8 @@ CREATE TABLE Reservation (
 
 -- Reviews table (weak entity)
 CREATE TABLE Reviews (
-    review_id INT PRIMARY KEY,
-    customer_id INT,
+    review_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    customer_id UUID,
     rating DECIMAL(2,1) CHECK (rating BETWEEN 1 AND 5),
     description VARCHAR(1000),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
