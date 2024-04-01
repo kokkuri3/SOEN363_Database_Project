804727c60c8376511121485844fd5bdcb4204f56
Sara Amri <93613026+kokkuri3@users.noreply.github.com>
2024-04-02 00:31:56 +0900

    Update TableCreationScript.sql
    Syntax Error with UUID so reverted IDs to INT

--- Phase_1/DDL/TableCreationScript.sql
+++ Phase_1/DDL/TableCreationScript.sql
@@ -1,6 +1,6 @@
 -- Car table
 CREATE TABLE Car (
-    car_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    car_id INT PRIMARY KEY,
     name VARCHAR(255),
     year INT,
     mpg INT,
@@ -9,7 +9,7 @@ CREATE TABLE Car (
 
 -- User table
 CREATE TABLE User (
-    user_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    user_id INT PRIMARY KEY,
     name VARCHAR(255),
     contact_number VARCHAR(15),
     email VARCHAR(255)
@@ -17,25 +17,25 @@ CREATE TABLE User (
 
 -- Customer table
 CREATE TABLE Customer (
-    customer_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
-    user_id UUID,
+    customer_id INT PRIMARY KEY,
+    user_id INT,
     premium BOOLEAN,
     FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
 -- Employee table
 CREATE TABLE Employee (
-    employee_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
-    user_id UUID,
+    employee_id INT PRIMARY KEY,
+    user_id INT,
     position ENUM('Manager', 'Salesperson', 'Mechanic'),
     FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
 -- Reservation table
 CREATE TABLE Reservation (
-    reservation_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
-    car_id UUID,
-    customer_id UUID,
+    reservation_id INT PRIMARY KEY,
+    car_id INT,
+    customer_id INT,
     rental_date DATE,
     return_date DATE,
     FOREIGN KEY (car_id) REFERENCES Car(car_id),
@@ -44,8 +44,8 @@ CREATE TABLE Reservation (
 
 -- Reviews table (weak entity)
 CREATE TABLE Reviews (
-    review_id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
-    customer_id UUID,
+    review_id INT PRIMARY KEY,
+    customer_id INT,
     rating DECIMAL(2,1) CHECK (rating BETWEEN 1 AND 5),
     description VARCHAR(1000),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
